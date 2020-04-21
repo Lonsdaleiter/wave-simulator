d80f6bc49b5f79d8851ee9da119bb14401f494ce
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-21 08:51:07 -0700

    more shader things

--- resources/shaders.metal
+++ resources/shaders.metal
@@ -6,22 +6,22 @@ struct UiVertexStruct {
 	float4 position;
 };
 
-struct UiFragmentStruct {
+struct UiFragmentInStruct {
     float4 position [[position]];
 };
 
 // vertex shader function
-vertex UiFragmentStruct vertex_ui(device UiVertexStruct* vertexArray [[ buffer(0) ]],
-                                  device float4 colour [[ buffer(1) ]]
+vertex UiFragmentInStruct vertex_ui(device UiVertexStruct *vertexArray [[ buffer(0) ]],
                                   unsigned int vid [[ vertex_id ]])
 {
-    UiFragmentStruct out;
+    UiFragmentInStruct out;
     out.position = vertexArray[vid].position;
     return out;
 }
 
 // fragment shader function
-fragment float4 fragment_ui(UiFragmentStruct in [[stage_in]])
+fragment float4 fragment_ui(UiFragmentInStruct in [[stage_in]],
+                            constant float4 &color [[ buffer(0) ]])
 {
-    return float4(1.0, 1.0, 1.0, 1.0);
+    return *color;
 }
--- src/wave/bundles/resource.rs
+++ src/wave/bundles/resource.rs
@@ -1,5 +1,10 @@
 use crate::wave::WaveApp;
-use cull_canyon::{set_layer_for_raw_window_handle, CAMetalLayer, MTLCommandQueue, MTLCompileOptions, MTLDevice, MTLRenderPipelineColorAttachmentDescriptor, MTLRenderPipelineDescriptor, MTLRenderPipelineState, MTLVertexDescriptor};
+use cull_canyon::{
+    set_layer_for_raw_window_handle, CAMetalLayer, MTLCommandQueue, MTLCompileOptions, MTLDevice,
+    MTLRenderPipelineColorAttachmentDescriptor, MTLRenderPipelineDescriptor,
+    MTLRenderPipelineState, MTLVertexAttributeDescriptor, MTLVertexBufferLayoutDescriptor,
+    MTLVertexDescriptor,
+};
 
 pub struct ResourceBundle {
     pub device: MTLDevice,
@@ -49,7 +54,29 @@ impl ResourceBundle {
                     desc.set_fragment_function(fragment);
                     desc.set_vertex_descriptor({
                         let desc = MTLVertexDescriptor::new();
-                        // TODO add the properties here
+                        let layouts = desc.get_layouts();
+                        layouts.set_object_at_indexed_subscript(
+                            {
+                                let desc = MTLVertexBufferLayoutDescriptor::new();
+                                desc.set_stride(16);
+                                desc.set_step_function(1); // per-vertex
+                                desc
+                            },
+                            0,
+                        );
+
+                        let attribs = desc.get_attributes();
+                        attribs.set_object_at_indexed_subscript(
+                            {
+                                let desc = MTLVertexAttributeDescriptor::new();
+                                desc.set_buffer_index(0);
+                                desc.set_offset(0);
+                                desc.set_format(31); // float4
+                                desc
+                            },
+                            0,
+                        );
+
                         desc
                     });
                     let color_attachments = desc.get_color_attachments();
