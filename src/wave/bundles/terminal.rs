a0e755c84453edf3e6351ef380321f54573dfdff
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-22 10:30:44 -0700

    parsing

--- src/wave/bundles/terminal.rs
+++ src/wave/bundles/terminal.rs
@@ -18,9 +18,30 @@ fn read_font_file(contents: &str, bundle: &WindowBundle) -> HashMap<char, Letter
     let mut letter_map = HashMap::new();
 
     contents.split("\r\n").for_each(|s: &str|{
+        let mut x = 0;
+        let mut y = 0;
+        let mut width = 0;
+        let mut height = 0;
+        let mut x_offset = 0;
+        let mut y_offset = 0;
+        let mut x_advance = 0;
         s.split(" ").for_each(|s: &str| {
             if !s.eq("") && !s.eq("char") {
                 println!("{}", s);
+                let k = s.split("=").collect::<Vec<_>>();
+                let item = k[0];
+                let val = k[1];
+                let value: &mut i32 = match item {
+                    "x" => &mut x,
+                    "y" => &mut y,
+                    "width" => &mut width,
+                    "height" => &mut height,
+                    "xoffset" => &mut x_offset,
+                    "yoffset" => &mut y_offset,
+                    "xadvance" => &mut x_advance,
+                    _ => return,
+                };
+                *value = val.parse().unwrap();
             }
         });
         println!();
