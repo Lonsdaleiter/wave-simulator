c41223f9e8d1b2d933a89bc5ddd9ae46b43111dc
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-21 19:51:44 -0700

    letters

--- src/wave/bundles/terminal.rs
+++ src/wave/bundles/terminal.rs
@@ -1,3 +1,17 @@
+static SPACE: Letter = Letter {
+    index: 0,
+    associated_char: Some(' '),
+};
+static CARET: Letter = Letter {
+    index: 0,
+    associated_char: None,
+};
+
+pub struct Letter {
+    pub index: u8,
+    pub associated_char: Option<char>,
+}
+
 pub struct TerminalBundle {
     //
 }
