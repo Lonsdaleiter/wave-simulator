6b08c4f08312bfe6b2d228cec3603007f06bb790
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-21 19:57:01 -0700

    aspect ratio

--- src/wave/bundles/terminal.rs
+++ src/wave/bundles/terminal.rs
@@ -1,14 +1,17 @@
 static SPACE: Letter = Letter {
     index: 0,
+    aspect_ratio: 2.0, // 2 / 1
     associated_char: Some(' '),
 };
 static CARET: Letter = Letter {
     index: 1,
+    aspect_ratio: 1.5, // 3 / 2
     associated_char: None,
 };
 
 pub struct Letter {
     pub index: u8,
+    pub aspect_ratio: f32,
     pub associated_char: Option<char>,
 }
 
