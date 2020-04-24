f9da68753136d6f0dc2c22df845b69a211e18c51
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-24 10:39:34 -0700

    thing

--- src/wave/view.rs
+++ src/wave/view.rs
@@ -1,7 +1,12 @@
 // TODO implement this properly
 
 pub struct Camera {
-    //
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub pitch: f32,
+    pub yaw: f32,
+    pub roll: f32,
 }
 
 impl Camera {
