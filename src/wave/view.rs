8d251f9254a56e3200287dd5e75a672a133ed21f
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-04-24 10:54:26 -0700

    dot products

--- src/wave/view.rs
+++ src/wave/view.rs
@@ -9,13 +9,23 @@ pub struct Camera {
     pub roll: f32,
 }
 
+fn dot(a: [f32; 3], b: [f32; 3]) -> f32 {
+    a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+}
+
 impl Camera {
     pub fn get_matrix(&self) -> [f32; 16] {
+        let cos_pitch = self.pitch.cos();
+        let sin_pitch = self.pitch.sin();
+        let cos_yaw = self.yaw.cos();
+        let sin_yaw = self.yaw.sin();
+        let cos_roll = self.roll.cos();
+        let sin_roll = self.roll.sin();
         [
-            1.0, 0.0, 0.0, 0.0, // r1
-            0.0, 1.0, 0.0, 0.0, // r2
-            0.0, 0.0, 1.0, 0.0, // r3
-            0.0, 0.0, 0.0, 1.0, // r4
+            cos_yaw, sin_yaw * sin_pitch, sin_yaw * cos_pitch, 0.0, // r1
+            0.0, cos_pitch, -sin_pitch, 0.0, // r2
+            -sin_yaw, cos_yaw * sin_pitch, cos_pitch * cos_yaw, 0.0, // r3
+            -dot(), -dot(), -dot(), 1.0, // r4 TODO finish up the dots
         ]
     }
 }
